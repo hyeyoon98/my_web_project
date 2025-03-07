64926304baf9be4d264e3df3f5c6489dc8a8cb72
hyeyoon98 <ymcacom665@gmail.com>
2025-03-07 17:39:02 +0900

    test

--- php/mypage.php
+++ php/mypage.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['user_id'])) {
 $user_id = $_SESSION['user_id'];
 
 // 현재 사용자 정보 가져오기
-$sql = "SELECT name, email FROM users WHERE user_id = ?";
+$sql = "SELECT name, email FROM users WHERE user_id = ? Limit 1";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
 $stmt->execute();
