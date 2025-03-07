a822959c4a1d663788e7c0738a56cdf24976b396
hyeyoon98 <ymcacom665@gmail.com>
2025-03-07 14:39:36 +0900

    오타 수정

--- php/write.php
+++ php/write.php
@@ -54,7 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="/css/style.css">
     <title>게시글 작성</title>
 </head>
 <body>
