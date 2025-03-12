99a80805be1a9113fd22b3ba3d05cfa97a9baeb3
hyeyoon98 <ymcacom665@gmail.com>
2025-03-12 10:00:06 +0900

    스타일 추가

--- php/post.php
+++ php/post.php
@@ -36,6 +36,7 @@ $is_admin = isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
     <title><?php echo htmlspecialchars($post['title']); ?></title>
 </head>
 <body>
